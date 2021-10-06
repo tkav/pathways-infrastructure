04e38748217df85d751cce4158778304ee9f5fa2
Tom Kavanagh <tom.kavanagh@contino.io>
2021-10-06 13:50:04 +1100

    Added depends on

--- modules/network/network.tf
+++ modules/network/network.tf
@@ -137,10 +137,13 @@ resource "aws_route_table_association" "public" {
   count          = var.public_subnet_count
   subnet_id      = aws_subnet.public_subnet[count.index].id
   route_table_id = aws_route_table.public[count.index].id
+
+  depends_on = [aws_subnet.public_subnet, aws_route_table.public]
+
 }
 
 resource "aws_route_table" "private" {
-  count         = var.public_subnet_count
+  count         = var.private_subnet_count
   vpc_id        = aws_vpc.vpc.id
 
   depends_on = [aws_nat_gateway.natgw]
@@ -161,4 +164,7 @@ resource "aws_route_table_association" "private" {
   count          = var.private_subnet_count
   subnet_id      = aws_subnet.private_subnet[count.index].id
   route_table_id = aws_route_table.private[count.index].id
+
+  depends_on = [aws_subnet.private_subnet, aws_route_table.private]
+
 }
