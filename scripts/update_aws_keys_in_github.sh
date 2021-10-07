f30be040ee3668ce89753a00046f0e6f1eed2e11
Tom Kavanagh <tom.kavanagh@contino.io>
2021-10-07 14:24:42 +1100

    Changed secrets from env

--- scripts/update_aws_keys_in_github.sh
+++ scripts/update_aws_keys_in_github.sh
@@ -10,9 +10,9 @@ function update_aws_keys_in_github() {
     echo "One or more AWS Environment Variables are not set."
     return
   else
-    gh secret set --env $env AWS_ACCESS_KEY_ID -b"${AWS_ACCESS_KEY_ID}"
-    gh secret set --env $env AWS_SECRET_ACCESS_KEY -b"${AWS_SECRET_ACCESS_KEY}"
-    gh secret set --env $env AWS_SESSION_TOKEN -b"${AWS_SESSION_TOKEN}"
+    gh secret set AWS_ACCESS_KEY_ID -b"${AWS_ACCESS_KEY_ID}"
+    gh secret set AWS_SECRET_ACCESS_KEY -b"${AWS_SECRET_ACCESS_KEY}"
+    gh secret set AWS_SESSION_TOKEN -b"${AWS_SESSION_TOKEN}"
   fi
 }
 
